0bdb8763563a6c99c3f243a568d772f9ea171280
DrAhmedAwni <dr.3aw3aw@gmail.com>
2026-06-06 17:13:34 +0300

    dokcer

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from 'next';
-import '@fontsource/montserrat';
+import '@fontsource/montserrat/400.css';
+import '@fontsource/montserrat/500.css';
+import '@fontsource/montserrat/600.css';
+import '@fontsource/montserrat/700.css';
 import './globals.css';
 import { Providers } from './providers';
 
